7ac7d658eb93c4eb98e8a5c50fbcf00df3ffa29e
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-04-10 08:51:39 +0000

    souhoukou tsuusin no test

--- src/udp_server_node.cpp
+++ src/udp_server_node.cpp
@@ -15,30 +15,38 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
     ros::Publisher publisher = n.advertise<std_msgs::Float32>("controller", 10);
 
-    int port = 8888;
+    int local_port = 8888;
+    int target_port = 8888;
 
     boost::asio::io_service io_service;
     io_service.run();
 
-    udp::socket sock(io_service, udp::endpoint(udp::v4(), port));
+    udp::socket udp_socket(io_service, udp::endpoint(udp::v4(), local_port));
 
-    ROS_INFO("UDP server started on localhost:%d", port);
+    ROS_INFO("UDP server started on localhost:%d", local_port);
 
     udp::endpoint remote_endpoint;
 
     while (ros::ok())
     {
       boost::array<char, 1024> recv_buf;
-      size_t len = sock.receive_from(boost::asio::buffer(recv_buf), remote_endpoint, 0);
+      size_t len = udp_socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint, 0);
       std::string recv_str(recv_buf.data(), len);
+
       ROS_INFO("Received data from %s:%d: %s", remote_endpoint.address().to_string().c_str(), remote_endpoint.port(), recv_str.c_str());
 
-      std_msgs::Float32 msg;
-      std::stringstream ss;
-      ss << recv_str;
-      msg.data = std::stof(ss.str());
-      publisher.publish(msg);
-      ros::spinOnce();
+        try {
+            std_msgs::Float32 msg;
+            std::stringstream ss;
+            ss << recv_str;
+            msg.data = std::stof(ss.str());
+            publisher.publish(msg);
+            udp_socket.send_to(boost::asio::buffer(recv_str), remote_endpoint);
+        } catch (char* e) {
+            udp_socket.send_to(boost::asio::buffer("Error: Please send Message that can be converted to foat."), remote_endpoint);
+        }
+            ros::spinOnce();
     }
+
     return 0;
 }
