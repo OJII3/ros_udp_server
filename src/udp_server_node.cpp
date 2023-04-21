560c3e132b736ae1a62a1241b757c228d07888fc
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-04-21 08:08:36 +0000

    add force cancel

--- src/udp_server_node.cpp
+++ src/udp_server_node.cpp
@@ -1,62 +1,90 @@
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/io_service.hpp>
-#include <boost/asio/ip/address.hpp>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
-#include <std_msgs/Float32.h>
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/array.hpp>
 
 using boost::asio::ip::udp;
 
+udp::endpoint 
+
+
+// Declare callback functions for UDP message reception and ROS message subscription
+void handle_receive(const boost::system::error_code& error,
+                    std::size_t bytes_transferred,
+                    udp::socket& socket,
+                    udp::endpoint& sender_endpoint,
+                    ros::Publisher& pub);
+void handle_subscribe(const std_msgs::String::ConstPtr& );
+
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "udp_server");
-    ros::NodeHandle n;
-    ros::Publisher publisher = n.advertise<std_msgs::Float32>("controller", 10);
-    
-    int local_port = 8888;
-    int target_port = 8888;
+    ros::init(argc, argv, "udp_ros_bridge");
+    ros::NodeHandle nh;
+    udp::endpoint sender_endpoint;
+    udp::endpoint target_endpoint;
 
+    // Subscribe to the ROS topic
+    ros::Publisher pub = nh.advertise<std_msgs::String>("controller", 1000);
+    ros::Subscriber sub = nh.subscribe<std_msgs::String>("controller", 1000, handle_subscribe);
+
+    // Set up UDP socket
     boost::asio::io_service io_service;
+    udp::socket socket(io_service, udp::endpoint(udp::v4(), 8888));
+
+    // Receive UDP messages and publish them to ROS topic
+    boost::array<char, 1024> recv_buffer;
+    socket.async_receive_from(boost::asio::buffer(recv_buffer), sender_endpoint, 0);
+    pub.publish(msg);
+
+    // Start the IO service
     io_service.run();
 
-    udp::socket udp_socket(io_service, udp::endpoint(udp::v4(), local_port));
-
-    ROS_INFO("UDP server started on localhost:%d", local_port);
-
-    udp::endpoint remote_endpoint;
-    
-    // Ctrl+Cを受信したら終了する
-    boost::asio::signal_set signals(io_service, SIGINT, SIGTERM);
-    signals.async_wait([&io_service](const boost::system::error_code& error, int signal_number)
-    {
-        if (!error)
-        {
-            io_service.stop();
-        }
-    });
-
-    while (ros::ok())
-    {
-      boost::array<char, 1024> recv_buf;
-      size_t len = udp_socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint, 0);
-      std::string recv_str(recv_buf.data(), len);
-
-      ROS_INFO("Received data from %s:%d: %s", remote_endpoint.address().to_string().c_str(), remote_endpoint.port(), recv_str.c_str());
-
-        try {
-            std_msgs::Float32 msg;
-            std::stringstream ss;
-            ss << recv_str;
-            msg.data = std::stof(ss.str());
-            publisher.publish(msg);
-            udp_socket.send_to(boost::asio::buffer(recv_str), remote_endpoint);
-        } catch (char* e) {
-            udp_socket.send_to(boost::asio::buffer("Error: Please send Message that can be converted to foat."), remote_endpoint);
-        }
-        
-        ros::spinOnce();
-    }
-
     return 0;
+}
+
+void handle_receive(const boost::system::error_code& error,
+                    boost::array<char, 1024>& recv_buffer,
+                    std::size_t bytes_transferred,
+                    udp::socket& socket,
+                    udp::endpoint& sender_endpoint,
+                    ros::Publisher& pub)
+{
+  if (!error || error == boost::asio::error::message_size)
+  {
+    // Get the target endpoint from the first received UDP message's IP
+    std::string target_ip = sender_endpoint.address().to_string();
+    udp::endpoint target_endpoint(boost::asio::ip::address::from_string(target_ip), 8888);
+
+    // Publish the received message to the ROS topic
+    std_msgs::String msg;
+    msg.data = std::string(recv_buffer.data(), bytes_transferred);
+    pub.publish(msg);
+
+    // Send the ROS message to the target endpoint
+    boost::asio::async_write(socket, boost::asio::buffer(msg.data),
+        boost::bind(&[](const boost::system::error_code& error,
+                        std::size_t bytes_transferred)
+        {
+          // Do nothing on send completion
+        }));
+  }
+
+  // Continue receiving UDP messages
+  socket.async_receive_from(boost::asio::buffer(recv_buffer),
+      sender_endpoint,
+      boost::bind(handle_receive, _1, _2, std::ref(socket), std::ref(sender_endpoint), std::ref(pub)));
+}
+
+void handle_subscribe(const std_msgs::String::ConstPtr& msg,
+                      udp::socket& socket,
+                      udp::endpoint& target_endpoint)
+{
+  // Send the ROS message to the target endpoint
+  boost::asio::async_write(socket, boost::asio::buffer(msg->data),
+      boost::bind(&[](const boost::system::error_code& error,
+                      std::size_t bytes_transferred)
+      {
+        // Do nothing on send completion
+      }));
 }
